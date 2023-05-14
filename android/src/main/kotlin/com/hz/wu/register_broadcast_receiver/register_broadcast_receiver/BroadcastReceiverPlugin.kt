21500c48eff4a993453660c5dcdcd4fa910f3461
Sathis Kumar <sathis.kumar@udemy.com>
2023-05-14 19:35:28 +0100

    add authority

--- android/src/main/kotlin/com/hz/wu/register_broadcast_receiver/register_broadcast_receiver/BroadcastReceiverPlugin.kt
+++ android/src/main/kotlin/com/hz/wu/register_broadcast_receiver/register_broadcast_receiver/BroadcastReceiverPlugin.kt
@@ -10,50 +10,56 @@ import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.EventChannel
 
 /** BroadcastReceiverPlugin */
-class BroadcastReceiverPlugin: FlutterPlugin, EventChannel.StreamHandler {
-  private lateinit var channel : EventChannel
-  private lateinit var receiver : BroadcastReceiver
-  private lateinit var context: Context
+class BroadcastReceiverPlugin : FlutterPlugin, EventChannel.StreamHandler {
+    private lateinit var channel: EventChannel
+    private lateinit var receiver: BroadcastReceiver
+    private lateinit var context: Context
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    context = flutterPluginBinding.applicationContext
-    channel = EventChannel(flutterPluginBinding.binaryMessenger, "register_broadcast_receiver")
-    channel.setStreamHandler(this)
-  }
-
-
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setStreamHandler(null)
-  }
-
-  override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
-    receiver = object : BroadcastReceiver() {
-      override fun onReceive(context: Context?, intent: Intent?) {
-        events?.success(mapOf(
-          "action" to intent?.action,
-          "data" to intent?.dataString,
-        ))
-      }
-    }
-    val intentFilter = IntentFilter()
-    intentFilter.priority = (arguments as Map<*, *>)["priority"] as Int
-    (arguments["actions"] as List<*>).forEach {
-      any ->      intentFilter.addAction(any as String)
-
-    }
-    (arguments["dataSchemes"] as List<*>).forEach {
-      any ->      intentFilter.addDataScheme(any as String)
-
-    }
-    (arguments["categories"] as List<*>).forEach {
-      any ->      intentFilter.addCategory(any as String)
-
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        context = flutterPluginBinding.applicationContext
+        channel = EventChannel(flutterPluginBinding.binaryMessenger, "register_broadcast_receiver")
+        channel.setStreamHandler(this)
     }
 
-    context.registerReceiver(receiver, intentFilter)
-  }
 
-  override fun onCancel(arguments: Any?) {
-    context.unregisterReceiver(receiver)
-  }
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setStreamHandler(null)
+    }
+
+    override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+        receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                events?.success(
+                    mapOf(
+                        "action" to intent?.action,
+                        "data" to intent?.dataString,
+                    )
+                )
+            }
+        }
+        val intentFilter = IntentFilter()
+        intentFilter.priority = (arguments as Map<*, *>)["priority"] as Int
+        (arguments["actions"] as List<*>).forEach { any ->
+            intentFilter.addAction(any as String)
+
+        }
+        (arguments["dataSchemes"] as List<*>).forEach { any ->
+            intentFilter.addDataScheme(any as String)
+
+        }
+        (arguments["categories"] as List<*>).forEach { any ->
+            intentFilter.addCategory(any as String)
+
+        }
+
+        (arguments["dataAuthorities"] as List<*>).forEach { any ->
+            intentFilter.addDataAuthority((any as String).split(",")[0], any.split(",").getOrNull(1))
+        }
+
+        context.registerReceiver(receiver, intentFilter)
+    }
+
+    override fun onCancel(arguments: Any?) {
+        context.unregisterReceiver(receiver)
+    }
 }
